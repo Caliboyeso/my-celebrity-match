27316e37634faf394c137368256a86a5672db7d0
Jesus Lopez <jesus.blopez97@gmail.com>
2020-09-17 11:33:22 -0700

    My Celebrity Match app completed and created with JavaScript, jQuery, Node.js, Express.js, body-parser, Bootstrap, Glyphicons.

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+  {
+    name: "Ryan Reynolds",
+    photo: "https://www.nydailynews.com/resizer/_ZDeRdPIK-GN3OFF1Qd-1_1yCj4=/415x311/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/MVTHQBKZQBHITEZ63BLOBUMPGE.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Scarlett Johansson",
+    photo: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d25026d34a5c400084addc9%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D3288%26cropY1%3D471%26cropY2%3D3758",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Samuel L Jackson",
+    photo: "https://pbs.twimg.com/profile_images/742877069793742848/c0Ec2mTU.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Jennifer Lawrence",
+    photo: "https://media1.popsugar-assets.com/files/thumbor/4fg9GOgyxSKwJme9GxiCCRxQVF8/364x0:4636x4272/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/08/28/717/n/1922398/274cf0545d66a7fd9b2956.87657545_/i/Jennifer-Lawrence.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Leonardo DiCaprio",
+    photo: "https://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Emma Watson",
+    photo: "https://vignette.wikia.nocookie.net/harrypotter/images/8/8c/Dhpc022.jpg/revision/latest/top-crop/width/360/height/450?cb=20120927223300",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name: "Denzel Washington",
+    photo: "https://www.looktothestars.org/photo/29-denzel-washington/story_half_width.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Margot Robbie",
+    photo: "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F585404d54bbe6f1f20e92308%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D744%26cropY1%3D58%26cropY2%3D802",
+    scores: [
+      "1",
+      "4",
+      "5",
+      "3",
+      "2",
+      "4",
+      "1",
+      "5",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Johnny Deep",
+    photo: "https://pbs.twimg.com/profile_images/1242973605337665536/2Ftp_t20_400x400.jpg",
+    scores: [
+      "3",
+      "1",
+      "3",
+      "4",
+      "1",
+      "3",
+      "5",
+      "4",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Angelina Jolie",
+    photo: "https://media1.popsugar-assets.com/files/thumbor/MfMPartvA5vZ1e_ouezU_MVDkvQ/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2018/07/13/976/n/1922398/b63ecaa95b4926f9984272.99170751_/i/Angelina-Jolie.jpg",
+    scores: [
+      "5",
+      "3",
+      "1",
+      "4",
+      "1",
+      "2",
+      "3",
+      "5",
+      "2",
+      "4"
+    ]
+  },
+
+];
+
+module.exports = friends;
